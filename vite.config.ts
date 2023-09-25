df8f6ed2da2e40578b9ec0f1a8399908796c6b60
dungle <quangdung100194@gmail.com>
2023-09-25 23:32:28 +0700

    integrate PWA with vite-pwa mode generateSW

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,84 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        theme_color: "#3587f6",
+        background_color: "#35f63d",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        name: "FriendScan",
+        short_name: "Friend",
+        description: "Build Friendscan with PWA",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "maskable_icon_x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable_icon_x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+      strategies: "generateSW",
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"], // define file types will be cached and managed by the Service Worker,
+        runtimeCaching: [
+          // Caching response in runtime
+          {
+            urlPattern: ({ url }) => {
+              return url.pathname.startsWith("/accounts");
+            },
+            method: "GET",
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "api-cache",
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+              expiration: {
+                maxEntries: 500,
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 1 month
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
+});
